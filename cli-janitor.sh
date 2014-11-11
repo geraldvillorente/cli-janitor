cc3cba716648c79826e574dbbc1d94a61449074a
Gerald Villorente <geraldvillorente@gmail.com>
2014-11-12 05:54:42 +0800

    Added script

---
+++ cli-janitor.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# The deeper you go to the filesystem the lesser space you left. 
+# This script makes sure your cli have enough space no matter how 
+# long you go deeper in your filesystem.
+#
+# @author
+#   Gerald Z. Villorente
+#
+# @date
+#   November 12, 2014
+#
+export PS1='\[\033[0;35m\]\u\[\033[0;33m\] \W\[\033[00m\]: '
